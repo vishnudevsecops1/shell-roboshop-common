cffdbeb7c488a405ff25e65f9a539f1e43df997b
vishnudevsecops1 <vvrraw123@gmail.com>
2026-02-08 14:34:01 +0000

    actions

---
+++ catalogue.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source ./common.sh
+app_name=catalogue
+
+check_root
+app_setup
+nodejs_setup
+systemd_setup
+
+# Loading data into MongoDB
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y &>>$LOGS_FILE
+
+INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOGS_FILE
+    VALIDATE $? "Loading products"
+else
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | Products already loaded ... $Y SKIPPING $N"
+fi
